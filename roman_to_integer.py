f83c335e5917a6c0bb8f6a260a6c6c57b25716d5
supermomie <fakhredinedev3.0@gmail.com>
2019-09-19 02:38:55 +0200

    check the positions of each number in the table and check if the numbers are smaller than the following ones

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,8 +1,7 @@
 class roman_to_integer:
     def romanToInt(self, s: str) -> int:
-        result = self.converToArabicNums(s)
-        print(result)
-        #return sum(result)
+        result = self.checkCombination(s)
+        return sum(result)
 
     def romanNumbers(self, val):
         roman_numbers = {"I" : 1,
@@ -16,7 +15,7 @@ class roman_to_integer:
         return matches
 
 
-    def converToArabicNums(self, string):
+    def converToIntegerNums(self, string):
         elements = list(string)
         number = []
         for i in range(len(elements)):
@@ -24,6 +23,15 @@ class roman_to_integer:
             num = next(r)[1]
             number.append(num)
         return number
+
+
+    def checkCombination(self, s):
+        translate = self.converToIntegerNums(s)
+        for i in range(len(translate)):
+            if i != (len(translate)-1):
+                if translate[i] < translate[i+1]:
+                    translate[i] = -translate[i]
+        return translate
 
 
 sol = roman_to_integer()
