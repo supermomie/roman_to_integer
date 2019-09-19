7a25e0597dd98bdeffb81a076ccb3357b9249604
supermomie <fakhredinedev3.0@gmail.com>
2019-09-19 02:49:56 +0200

    UPDATE README

---
+++ README.mk
@@ -0,0 +1,32 @@
+[link1]: https://leetcode.com/problems/roman-to-integer/ "Roman to integer"
+
+# ROMAN TO INTEGER
+
+[https://leetcode.com/problems/integer-to-roman/][link1]
+
+
+###problem
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+```
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+```
+
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example :
+```
+Input: "MCMXCIV"
+
+Output: 1994
+
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+```
