b118f98cb3aae0944047fe0b017134d08a0ad5df
supermomie <fakhredinedev3.0@gmail.com>
2019-09-19 02:18:03 +0200

    define Roman numbers and convert it to arabic numbers

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,3 +1,25 @@
 class roman_to_integer:
     def romanToInt(self, s: str) -> int:
+        
+    def romanNumbers(self, val):
+        roman_numbers = {"I" : 1,
+                        "V" : 5,
+                        "X" : 10,
+                        "L" : 50,
+                        "C" : 100,
+                        "D" : 500,
+                        "M" : 1000}
+        matches = ([x, y] for x, y in roman_numbers.items() if val == x)
+        return matches
+
+
+    def converToArabicNums(self, string):
+        elements = list(string)
+        number = []
+        for i in range(len(elements)):
+            r = self.romanNumbers(elements[i])
+            num = next(r)[1]
+            number.append(num)
+        return number
+
 
