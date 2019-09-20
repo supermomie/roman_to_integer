94f90ddd11d248c61162b1e73fa63fe44a9638d1
supermomie <fakhredinedev3.0@gmail.com>
2019-09-20 19:15:43 +0200

    README.mkd

--- optimization.py
+++ optimization.py
@@ -1,16 +1,30 @@
+from datetime import datetime
+
 def romanToInt(s):
         roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         init_val = 0
         for i in range(len(s)):
-            print(roman_numbers[s[i]])
+            #print(roman_numbers[s[i]])
             if i > 0 and roman_numbers[s[i]] > roman_numbers[s[i - 1]]:
-                print(roman_numbers[s[i]], "---", roman_numbers[s[i - 1]])
-                print(roman_numbers[s[i]] - 2 * roman_numbers[s[i - 1]])
+                #print(roman_numbers[s[i]], "---", roman_numbers[s[i - 1]])
+                #print(roman_numbers[s[i]] - 2 * roman_numbers[s[i - 1]])
                 init_val += roman_numbers[s[i]] - 2 * roman_numbers[s[i - 1]]
-                print("init_val " ,init_val)
+                #print("init_val " ,init_val)
             else:
                 init_val += roman_numbers[s[i]]
         return init_val
 
 
-print(romanToInt("MCMXCII"))
+
+
+start = datetime.now()
+
+romanToInt("MCMXCII")
+print("one time : ", datetime.now()-start)
+
+for i in range(100000):
+    
+   res= romanToInt("MCMXCII")
+
+print(res)
+print("looped 1M time : ",datetime.now()-start) #0.1s
