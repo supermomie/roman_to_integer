5bc0a405a02373edafbcd9c0ddf22c5a181f6239
supermomie <fakhredinedev3.0@gmail.com>
2019-09-20 04:23:55 +0200

    optimizations

---
+++ optimization.py
@@ -0,0 +1,16 @@
+def romanToInt(s):
+        roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        init_val = 0
+        for i in range(len(s)):
+            print(roman_numbers[s[i]])
+            if i > 0 and roman_numbers[s[i]] > roman_numbers[s[i - 1]]:
+                print(roman_numbers[s[i]], "---", roman_numbers[s[i - 1]])
+                print(roman_numbers[s[i]] - 2 * roman_numbers[s[i - 1]])
+                init_val += roman_numbers[s[i]] - 2 * roman_numbers[s[i - 1]]
+                print("init_val " ,init_val)
+            else:
+                init_val += roman_numbers[s[i]]
+        return init_val
+
+
+print(romanToInt("MCMXCII"))
